01216a76296b45592308819398ffca4c53d63f51
runghall <173676704@qq.com>
2017-03-27 11:18:33 +0800

    inital

---
+++ client_test/src/test/java/com/runghall/EurekaClientApplicationTests.java
@@ -0,0 +1,18 @@
+package com.runghall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		String a[] = new String[]{"1aa", "da", "daf"};
+		Object o = new Object[]{};
+	}
+
+}
---
+++ server_cluster/src/main/java/com/runghall/EurekaServerClusterApplication.java
@@ -0,0 +1,15 @@
+package com.runghall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerClusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerClusterApplication.class, args);
+	}
+}
